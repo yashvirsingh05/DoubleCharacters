1b9b8507b2a1aec031164868416256dc82d22036
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-12 00:38:09 -0700

    complete program

--- DoubleCharacter.java
+++ DoubleCharacter.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class DoubleCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i) + "" + s.charAt(i));
+        }
+    }
 }
